f48656655cfc9b788195e555dec97e560b7c2cab
pwmcintyre <peter.mcintyre@gmail.com>
2021-04-05 22:43:53 +1000

    wip discord

--- README.md
+++ README.md
@@ -35,7 +35,7 @@ aws cloudformation deploy \
 
 #### Optional: Deploy discord addon
 
-Requires [AWS SAM CLI](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install.html)
+Requires [AWS SAM CLI](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install.html) (or use CloudShell)
 
 ```shell
 sam build && sam deploy --guided
---
+++ scripts/discord_command.js
@@ -0,0 +1,91 @@
+const fetch = require('node-fetch')
+async function main (api, config, token) {
+
+    const body = JSON.stringify(config)
+    console.log("posting", {api, body})
+    const response = await fetch(api, {
+        method: 'post',
+        body,
+        headers: {
+            'Authorization': 'Bot ' + token,
+            'Content-Type': 'application/json'
+        }
+    })
+    const json = await response.json()
+
+    console.log("response", JSON.stringify(json))
+}
+
+const token = process.env.DISCORD_TOKEN
+const bot = process.env.DISCORD_BOT
+const guild = process.env.DISCORD_GUILD
+const api = `https://discord.com/api/v8/applications/${ bot }/guilds/${ guild }/commands`
+
+const config = {
+    "name": "server",
+    "description": "control your server",
+    "options": [
+        {
+            "name": "start",
+            "description": "Start server",
+            "type": 1 // 1 is type SUB_COMMAND
+        },
+        {
+            "name": "stop",
+            "description": "Stop server",
+            "type": 1
+        },
+        {
+            "name": "get",
+            "description": "Get details",
+            "type": 2, // 2 is type SUB_COMMAND_GROUP
+            "options": [
+                {
+                    "name": "ip",
+                    "description": "Get public IP",
+                    "type": 1
+                }
+            ]
+        }
+    ]
+}
+
+const config2 = {
+  "name": "penguin",
+  "description": "Send a GIF of a penguin",
+  "options": [
+      {
+          "name": "breed",
+          "description": "The breed of penguin",
+          "type": 3,
+          "required": true,
+          "choices": [
+              {
+                  "name": "Magellanic",
+                  "value": "magellanic"
+              },
+              {
+                  "name": "Emperor",
+                  "value": "emperor"
+              },
+              {
+                  "name": "Chinstrap",
+                  "value": "chinstrap"
+              },
+              {
+                  "name": "Gentoo",
+                  "value": "gentoo"
+              }
+          ]
+      },
+      {
+          "name": "stickers",
+          "description": "Whether to show only stickers",
+          "type": 5,
+          "required": false
+      }
+  ]
+}
+
+
+main(api, config, token)
